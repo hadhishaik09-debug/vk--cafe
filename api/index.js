20c3d0589a48e4c3000267e48ebfe6492f836dd6
Hadhi Shaik <hadhishaik09@gmail.com>
2026-05-09 17:05:22 +0530

    Fixed TanStack server export

--- api/index.js
+++ api/index.js
@@ -1,5 +1,13 @@
-import handler from "../dist/server/server.js";
+import * as server from "../dist/server/server.js";
 
-export default async function (req, res) {
-    return handler(req, res);
+export default async function handler(req, res) {
+    if (typeof server.default === "function") {
+        return server.default(req, res);
+    }
+
+    if (typeof server.handler === "function") {
+        return server.handler(req, res);
+    }
+
+    throw new Error("No valid server handler found");
 }
